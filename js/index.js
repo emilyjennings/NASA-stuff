a14de1fac0234ee349dc0dcb78fa62af22be454e
github username <github email address>
2019-02-14 22:32:29 -0500

    made some other pages

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+  function random(num) {
+    return Math.floor(Math.random() * num);
+  }
+
   $('#searchbutton').click(function(){
 
     const params = $('input').val()
@@ -11,24 +15,47 @@ $(document).ready(function(){
       dataType : "json",
     }).done(function(json){
 
-        let img = '<img src="' + json.collection.items[0].links[0].href + '">'
-        let desc = json.collection.items[0].data[0].description
+        let listlen = json.collection.items.length
+        let randomNum = random(listlen)
+        let img = '<img src="' + json.collection.items[randomNum].links[0].href + '">'
+        let desc = json.collection.items[randomNum].data[0].description
+        let title = json.collection.items[randomNum].data[0].title
 
         if (img.includes('jpg'))
           $(".image").html(img)
         else {
           for (var i =0; i < 20; i++){
             let nextimg = json.collection.items[i].links[0].href
+            let img = '<img src="' + nextimg + '">'
             if (nextimg.includes('jpg')){
-              $(".image").html('<img src="' + nextimg + '">')
+              $(".image").html(img)
             }
           }
         }
 
         $('.desc').text(desc)
+        $('.title').text(title)
     });
+  });
+
+  $(".nav").hide();
+  $("#hamburger").click(function(){
+    $(".nav").slideToggle("slow", function(){
+      $("#hamburger").hide();
+      $(".nav").show();
+      $("#cross").show();
+    });
+  });
 
 
-
+  $("#cross").click(function(event){
+    event.preventDefault();
+    //click event that takes information from the event and prevents refresh
+    if ($("#cross").is(":visible")){
+      $(".nav").slideToggle("slow", function(){
+        $("#cross").hide();
+        $("#hamburger").show();
+      });
+    }
   });
 });
