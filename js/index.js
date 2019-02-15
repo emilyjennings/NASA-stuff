24a7e8b78751abf36e3db16b56468ae63c89877b
github username <github email address>
2019-02-15 10:33:55 -0500

    adding the first game, organized files

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 
   $('#searchbutton').click(function(){
 
+    //getting the input from the user and sending it to the API
     const params = $('input').val()
     var url = "https://images-api.nasa.gov/search?q="
 
@@ -15,15 +16,18 @@ $(document).ready(function(){
       dataType : "json",
     }).done(function(json){
 
+        //getting all the items out of the API that I need
         let listlen = json.collection.items.length
         let randomNum = random(listlen)
         let img = '<img src="' + json.collection.items[randomNum].links[0].href + '">'
         let desc = json.collection.items[randomNum].data[0].description
         let title = json.collection.items[randomNum].data[0].title
 
+        //the logic in the game in case the first result isn't an image
         if (img.includes('jpg'))
           $(".image").html(img)
         else {
+          //if the first random result isn't an image, it goes through the results to find one that is
           for (var i =0; i < 20; i++){
             let nextimg = json.collection.items[i].links[0].href
             let img = '<img src="' + nextimg + '">'
@@ -38,6 +42,7 @@ $(document).ready(function(){
     });
   });
 
+  //the hamburger nav menu
   $(".nav").hide();
   $("#hamburger").click(function(){
     $(".nav").slideToggle("slow", function(){
@@ -47,7 +52,7 @@ $(document).ready(function(){
     });
   });
 
-
+  //the animation for the hmaburger menu going back to a box
   $("#cross").click(function(event){
     event.preventDefault();
     //click event that takes information from the event and prevents refresh
@@ -58,4 +63,11 @@ $(document).ready(function(){
       });
     }
   });
+
+  $('.playbutton').click(function(event){
+    //displaying the game results on page when the play button is clicked
+    event.preventDefault();
+    $('.namegame')
+  });
+
 });
